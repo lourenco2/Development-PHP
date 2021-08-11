3892ab24dcf1c636ffae92d1d0580aaa052c1918
Gabriel Lourenço <75556848+lourenco2@users.noreply.github.com>
2021-08-10 21:48:15 -0300

    Delete PHP directory

--- PHP/lock.php
+++
@@ -1,6 +0,0 @@
-<?php session_start(); // iniciar sessão
-if(empty($_SESSION)) // se não há variável de sessão registrada
-{
-	header('location:login.php?msg=notlog'); // redirecinar para pag de login
-}
-?>
